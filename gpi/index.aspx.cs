f38d7d2ed372774447cb958f64688294e5e2d902
Brad Bowes <bbowes@junewarren-nickles.com>
2015-04-27 14:44:27 -0600

    garmin new template

--- gpi/index.aspx.cs
+++ gpi/index.aspx.cs
@@ -2,16 +2,22 @@
 using System.Configuration;
 using GarminUnlock;
 using Ajax;
+using NAuth2;
+using System.Web;
 
 public partial class index : System.Web.UI.Page {
   
-  private string _product = ConfigurationManager.AppSettings["Garmin.Product"];
-  private string _filename = ConfigurationManager.AppSettings["Garmin.FileName"];
-  private string _source = ConfigurationManager.AppSettings["Garmin.Source"];
-  private string _unlockSource = ConfigurationManager.AppSettings["Garmin.UnlockSource"];
-  private string _gmaUnlockSource = ConfigurationManager.AppSettings["Garmin.GMAUnlockSource"];
-  private string _controlDomain = ConfigurationManager.AppSettings["Garmin.ControlDomain"];
-  private string _controlUnlock = ConfigurationManager.AppSettings["Garmin.ControlUnlock"];
+  private static string getSetting(string key) {
+    return ConfigurationManager.AppSettings[key].ToString();
+  }
+
+  private string _product = getSetting("Garmin.Product");
+  private string _filename = getSetting("Garmin.FileName");
+  private string _source = getSetting("Garmin.Source");
+  private string _unlockSource = getSetting("Garmin.UnlockSource");
+  private string _gmaUnlockSource = getSetting("Garmin.GMAUnlockSource");
+  private string _controlDomain = getSetting("Garmin.ControlDomain");
+  private string _controlUnlock = getSetting("Garmin.ControlUnlock");
 
   protected string FileName { get { return _filename; } }
   protected string Source { get { return _source; } }
@@ -33,5 +39,72 @@ public partial class index : System.Web.UI.Page {
     catch(Exception ex) {
       return ex.Message;
     }
+  }
+
+  private AuthenticationClient getClient() {
+    return AuthenticationClient.getClient();
+  }
+
+  private void setCookies(AuthenticationReply reply) {
+    HttpCookie cookie = new HttpCookie("NDID");
+    cookie.Value = reply.Device;
+    cookie.Expires = DateTime.Now.AddDays(90);
+    Response.Cookies.Add(cookie);      
+    
+    cookie = new HttpCookie("NUID");
+    cookie.Value = reply.CookieText;
+    cookie.Expires = DateTime.Now.AddDays(15);
+    Response.Cookies.Add(cookie);      
+  }
+
+  private string cookieValue(string name) {
+    HttpCookie cookie = Request.Cookies[name];
+    return cookie == null ? "" : cookie.Value;
+  }
+
+  override protected void OnInit(EventArgs e) {
+
+      string device = "";
+      string cookie = "";
+      AuthenticationReply reply;
+      AuthenticationClient client = getClient();
+
+      try {
+
+        String token = (String) Request["ntoken"];
+
+        if (token == null) {
+          string requestUrl = Request.Url.ToString();
+          Session["RequestUrl"] = requestUrl;
+
+          cookie = cookieValue("NUID");
+          device = cookieValue("NDID");
+
+          reply = client.Authenticate(cookie,
+                                      device,
+                                      getSetting("Authentication.PubCode"));
+
+          if (reply.Action == "CHECKID") {
+            Response.Redirect(
+                getSetting("Authentication.CheckUrl") +
+                "?pub=" + getSetting("Authentication.PubCode") +
+                "&continue=" + Server.UrlEncode(requestUrl));
+          } else {
+            setCookies(reply);
+          }
+                
+        } else {
+          reply = client.DecodeToken(token);
+          setCookies(reply);
+          Response.Redirect(Session["RequestUrl"].ToString());
+        }
+
+        if (reply.Action != "ALLOW")
+          Response.Redirect(
+              getSetting("Authentication.LoginUrl") +
+              "?pub=" + getSetting("Authentication.PubCode") +
+              "&continue=" + Server.UrlEncode(Request.Url.ToString()));
+
+      } catch (Exception ex) {     }
   }
 }
